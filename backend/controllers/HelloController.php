feacbeb4821b4286e754150ab97ee2cd5eb38761
cesar <cesar_romero.dev@outlook.com>
2021-01-27 10:00:36 -0600

    tercercommit

---
+++ backend/controllers/HelloController.php
@@ -0,0 +1,16 @@
+<?php
+/** User: TheCodeholic ... */
+
+namespace backend\controllers;
+
+use yii\web\Controller;
+
+
+class HolaController extends Controller 
+{
+    
+    public function actionIndex()
+    {
+        return "Hello World";
+    }
+}
